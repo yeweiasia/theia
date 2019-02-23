c180d870ba59be938ef0699977face1373034280
yewei <yeweiasia@gmail.com>
2019-02-23 23:42:13 +0800

    add start debug for local branch

---
+++ start-debug.sh
@@ -0,0 +1,1 @@
+/home/yewei/Programs/nodejs/node-v8.15.0-linux-x64/bin/node --inspect examples/browser/src-gen/backend/main.js --hostname=0.0.0.0 --port=3000 --no-cluster --app-project-path=/home/yewei/Workspace/theia/examples/browser --no-app-auto-install
